7d494e19802acc102bf1712567ee7d78faff5bc2
GabrielJoseC <42181447+GabrielJoseC@users.noreply.github.com>
2018-12-05 22:01:11 -0200

    Update MenuBack.cs

--- Avangar/Assets/Script/MenuBack.cs
+++ Avangar/Assets/Script/MenuBack.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuBack : MonoBehaviour {
-
+//volta para o menu
     public void Back()
     {
         SceneManager.LoadScene("Menu");
